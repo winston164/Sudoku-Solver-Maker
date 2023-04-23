c30bf3caa62a5987a10da927a34b43b509e16c36
Antonio Vissoni <92328741+techwithantonio@users.noreply.github.com>
2023-04-23 19:44:32 +0800

    Refactor with clear variable names

--- sudoku/src/App.tsx
+++ sudoku/src/App.tsx
@@ -1,69 +1,75 @@
+// Import the useState hook from the 'react' module
 import { useState } from 'react';
+// Import the 'App.css' stylesheet
 import './App.css';
 
-// Define a type for a SudokuMatrix which is a 2D array of numbers.
+// Define a type alias for a Sudoku matrix
 type SudokuMatrix = number[][];
 
-// Define an initial SudokuMatrix.
-const initialMatrix: SudokuMatrix = [
-  [5, 3, 0, 0, 7, 0, 0, 0, 0],
-  [6, 0, 0, 1, 9, 5, 0, 0, 0],
-  [0, 9, 8, 0, 0, 0, 0, 6, 0],
-  [8, 0, 0, 0, 6, 0, 0, 0, 3],
-  [4, 0, 0, 8, 0, 3, 0, 0, 1],
-  [7, 0, 0, 0, 2, 0, 0, 0, 6],
-  [0, 6, 0, 0, 0, 0, 2, 8, 0],
-  [0, 0, 0, 4, 1, 9, 0, 0, 5],
-  [0, 0, 0, 0, 8, 0, 0, 7, 9],
+// Define an initial Sudoku matrix
+const initialSudokuMatrix: SudokuMatrix = [
+[5, 3, 0, 0, 7, 0, 0, 0, 0],
+[6, 0, 0, 1, 9, 5, 0, 0, 0],
+[0, 9, 8, 0, 0, 0, 0, 6, 0],
+[8, 0, 0, 0, 6, 0, 0, 0, 3],
+[4, 0, 0, 8, 0, 3, 0, 0, 1],
+[7, 0, 0, 0, 2, 0, 0, 0, 6],
+[0, 6, 0, 0, 0, 0, 2, 8, 0],
+[0, 0, 0, 4, 1, 9, 0, 0, 5],
+[0, 0, 0, 0, 8, 0, 0, 7, 9],
 ];
 
-// Define the App component.
+// Define the App component
 function App() {
-  // Use the useState hook to set up a state variable for the SudokuMatrix
-  const [matrix] = useState<SudokuMatrix>(initialMatrix);
-  // Use the useState hook to set up a state variable for the highlighted cell
-  const [highlightedCell, setHighlightedCell] = useState<number[]>([]);
+// Define a state variable for the Sudoku matrix
+const [sudokuMatrix] = useState<SudokuMatrix>(initialSudokuMatrix);
+// Define a state variable for the highlighted cell
+const [highlightedCell, setHighlightedCell] = useState<[number, number]>([-1, -1]);
 
-  // Define a function that renders a row of the Sudoku matrix
-  const renderRow = (row: number[], rowIndex: number) => {
-    return (
-      <div className="sudoku-row" key={rowIndex}>
-        {row.map((cell, cellIndex) => {
-          // Determine if the cell should be highlighted
-          const isHighlighted =
-            highlightedCell[0] === rowIndex || highlightedCell[1] === cellIndex;
-          // Set the appropriate class for the cell
-          const cellClass = `sudoku-cell${isHighlighted ? ' highlighted' : ''}`;
-          // Render the cell
-          return (
-            <div
-              key={cellIndex}
-              className={cellClass}
-              onMouseEnter={() => setHighlightedCell([rowIndex, cellIndex])}
-              onMouseLeave={() => setHighlightedCell([])}
-            >
-              {cell === 0 ? '' : cell}
-            </div>
-          );
-        })}
-      </div>
-    );
-  };
+// Define a function that renders a row of cells in the Sudoku grid
+const renderRow = (row: number[], rowIndex: number) => {
+return (
+<div className="sudoku-row" key={rowIndex}>
+{row.map((cell, cellIndex) => {
+// Check if the cell is highlighted
+const isCellHighlighted = highlightedCell[0] === rowIndex || highlightedCell[1] === cellIndex;
+// Define the class name for the cell
+const cellClassName = sudoku-cell${isCellHighlighted ? ' highlighted' : ''};
+// Define the value of the cell
+const cellValue = cell === 0 ? '' : cell;
+return (
+<div
+key={cellIndex}
+className={cellClassName}
+onMouseEnter={() => setHighlightedCell([rowIndex, cellIndex])}
+onMouseLeave={() => setHighlightedCell([-1, -1])}
+>
+{cellValue}
+</div>
+);
+})}
+</div>
+);
+};
 
-  // Define a function that renders the entire Sudoku matrix
-  const renderSudoku = () => {
-    return (
-      <div className="sudoku">
-        {matrix.map((row, index) => (
-          <div key={index}>{renderRow(row, index)}</div>
-        ))}
-      </div>
-    );
-  };
+// Define a function that renders the Sudoku grid
+const renderSudoku = () => {
+return (
+<div className="sudoku">
+{sudokuMatrix.map((row, rowIndex) => (
+<div key={rowIndex}>{renderRow(row, rowIndex)}</div>
+))}
+</div>
+);
+};
 
-  // Render the Sudoku matrix inside a div
-  return <div>{renderSudoku()}</div>;
+// Render the Sudoku grid inside a container
+return (
+<div>
+{renderSudoku()}
+</div>
+);
 }
 
-// Export the App component as the default export
+// Export the App component as the default export of this module
 export default App;
