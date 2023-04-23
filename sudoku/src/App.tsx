efd099291001e5c4068f2c617204e2ce7b43df9b
Antonio Vissoni <92328741+techwithantonio@users.noreply.github.com>
2023-04-23 17:38:45 +0800

    Commented code. Renamed some variables for clarity

--- sudoku/src/App.tsx
+++ sudoku/src/App.tsx
@@ -1,10 +1,10 @@
 import { useState } from 'react';
 import './App.css';
 
+// Define a type for a SudokuMatrix which is a 2D array of numbers.
 type SudokuMatrix = number[][];
 
-// add setMatrix function later
-
+// Define an initial SudokuMatrix.
 const initialMatrix: SudokuMatrix = [
   [5, 3, 0, 0, 7, 0, 0, 0, 0],
   [6, 0, 0, 1, 9, 5, 0, 0, 0],
@@ -17,17 +17,24 @@ const initialMatrix: SudokuMatrix = [
   [0, 0, 0, 0, 8, 0, 0, 7, 9],
 ];
 
+// Define the App component.
 function App() {
+  // Use the useState hook to set up a state variable for the SudokuMatrix
   const [matrix] = useState<SudokuMatrix>(initialMatrix);
+  // Use the useState hook to set up a state variable for the highlighted cell
   const [highlightedCell, setHighlightedCell] = useState<number[]>([]);
 
+  // Define a function that renders a row of the Sudoku matrix
   const renderRow = (row: number[], rowIndex: number) => {
     return (
       <div className="sudoku-row" key={rowIndex}>
         {row.map((cell, cellIndex) => {
+          // Determine if the cell should be highlighted
           const isHighlighted =
             highlightedCell[0] === rowIndex || highlightedCell[1] === cellIndex;
+          // Set the appropriate class for the cell
           const cellClass = `sudoku-cell${isHighlighted ? ' highlighted' : ''}`;
+          // Render the cell
           return (
             <div
               key={cellIndex}
@@ -43,6 +50,7 @@ function App() {
     );
   };
 
+  // Define a function that renders the entire Sudoku matrix
   const renderSudoku = () => {
     return (
       <div className="sudoku">
@@ -53,7 +61,9 @@ function App() {
     );
   };
 
+  // Render the Sudoku matrix inside a div
   return <div>{renderSudoku()}</div>;
 }
 
+// Export the App component as the default export
 export default App;
